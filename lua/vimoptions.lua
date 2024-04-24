0c1c48df1f4ec2ee3089bd85fa943ffed43f383b
unknown <inithin683@gmail.com>
2024-04-25 00:01:30 +0530

    disable folding

--- lua/vimoptions.lua
+++ lua/vimoptions.lua
@@ -5,6 +5,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
 vim.cmd("set number")
+vim.cmd("set nofoldenable")
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
 
